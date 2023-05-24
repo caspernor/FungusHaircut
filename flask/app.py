c1c348351301580b28c0d6ed8a57a9094b089eb8
Casper Nordeidet <caspernordeidet@gmail.com>
2023-05-24 22:36:19 +0200

    flask app

--- flask/app.py
+++ flask/app.py
@@ -11,15 +11,17 @@ class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_name = db.Column(db.String(50))
     customer_email = db.Column(db.String(50))
+    customer_phone = db.Column(db.String(20))
     date = db.Column(db.DateTime)
 
 @app.route('/booking', methods=['POST'])
 def booking():
     customer_name = request.form.get('customer_name')
     customer_email = request.form.get('customer_email')
+    customer_phone = request.form.get('customer_phone')
     date = request.form.get('date')
 
-    booking = Booking(customer_name=customer_name, customer_email=customer_email, date=date)
+    booking = Booking(customer_name=customer_name, customer_email=customer_email, customer_phone=customer_phone, date=date)
     db.session.add(booking)
     db.session.commit()
 
