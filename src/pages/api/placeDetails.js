9764c836d88b52ad1a0802dcadb1c9066553927c
Casper Nordeidet <caspernordeidet@gmail.com>
2023-05-24 17:11:06 +0200

    initial commit 2

--- src/pages/api/placeDetails.js
+++ src/pages/api/placeDetails.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 export default async function handler(req, res) {
   const googlePlaceId = "ChIJi5a4zd9QWARADhjzldYQRy4";
-  const googlePlacesKey = "AIzaSyBb7cSmhkDswhQhebDrwTkmK1zmPfZEUHg";
+  const googlePlacesKey = process.env.API_KEY;
   const googlePlacesUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${googlePlaceId}&key=${googlePlacesKey}`;
 
   const response = await fetch(googlePlacesUrl);
