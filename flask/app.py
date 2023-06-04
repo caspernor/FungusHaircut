d5a4cf19e01cb7f9f55d7d2b34a41cf1fa67e0b0
Casper Nordeidet <caspernordeidet@gmail.com>
2023-06-04 15:09:05 +0200

    Flask app

--- flask/app.py
+++ flask/app.py
@@ -1,96 +1,3 @@
-"""
-from flask import Flask, request
-import pyodbc
-from datetime import datetime
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-server = 'fungusbooking.database.windows.net'
-database = 'FungusBooking'
-username = 'CloudSA80f32b97'
-password = 'Fungushaircut100'
-driver = '{ODBC Driver 17 for SQL Server}'  # Update the driver based on your system
-
-connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
-connection = pyodbc.connect(connection_string)
-
-class Booking:
-    def __init__(self, id, customer_name, customer_email, date):
-        self.id = id
-        self.customer_name = customer_name
-        self.customer_email = customer_email
-        self.date = date
-
-@app.route('/', methods=['GET'])
-def getIndex():
-    return 'Hejhej Backend'
-
-@app.route('/bookings', methods=['GET'])
-def getBookings():
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM booking')
-    bookings = []
-    for row in cursor.fetchall():
-        booking = Booking(row.id, row.customer_name, row.customer_email, row.date)
-        bookings.append({
-            'id': booking.id,
-            'customer_name': booking.customer_name,
-            'customer_email': booking.customer_email,
-            'date': str(booking.date)
-        })
-    return {'bookings': bookings}
-
-@app.route('/booking', methods=['POST'])
-def createBooking():
-    customer_name = request.form.get('customer_name')
-    customer_email = request.form.get('customer_email')
-    date = request.form.get('date')
-
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO booking (customer_name, customer_email, date) VALUES (?, ?, ?)',
-                   (customer_name, customer_email, date))
-    connection.commit()
-
-    return 'Booking successful!'
-
-AVAILABLE_TIME_SLOTS = [
-    "10:00",
-    "10:30",
-    "11:00",
-    "11:30",
-    "12:00",
-    "12:30",
-    "13:00",
-    "13:30",
-    "14:00",
-    "14:30",
-    "15:00",
-    "15:30",
-    "16:00",
-    "16:30",
-    "17:00",
-    "17:30",
-  ]
-
-@app.route('/timeslot/<date>', methods=['GET'])
-def getAvailableTimeslots(date):
-    requested_date = datetime.strptime(date, "%Y-%m-%d")
-    cursor = connection.cursor()
-    cursor.execute('SELECT date FROM booking WHERE date = ?', requested_date.date())
-    booked_timeslots = [str(row.date) for row in cursor.fetchall()]
-    available_timeslots = [slot for slot in AVAILABLE_TIME_SLOTS if slot not in booked_timeslots]
-
-    return {'available_timeslots': available_timeslots}
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
-
-
-
-
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -99,15 +6,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-
 server = 'fungusbooking.database.windows.net'
 database = 'FungusBooking'
 username = 'CloudSA80f32b97'
 password = 'Fungushaircut100'
-driver = '{ODBC Driver 18 for SQL Server}'  # Update the driver based on your system
-
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
+driver = '{ODBC Driver 18 for SQL Server}'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mssql+pymssql://{username}:{password}@{server}/{database}?driver={driver}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
